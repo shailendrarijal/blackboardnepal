9248b57edf92e7f240902bcb911dc0e17147f120
shailendrarijal <45778804+shailendrarijal@users.noreply.github.com>
2020-06-26 08:13:16 +1000

    update index.php
    added GTM

--- index.php
+++ index.php
@@ -1,54 +1,62 @@
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJB6J4S');</script>
+<!-- End Google Tag Manager -->
+
 	<link rel="stylesheet" type="text/css" href="blackboardnepal.css">
+	<meta charset="UTF-8">
+    <meta name="description" content="Online community platform for Nepalese communities">
+    <meta name="keywords" content="NAATI, NAATI CCL, NAATI CCL NEPALI, NEPALI LANGUAGE, NEPALI, NEPALI COMMUNITY, FREE RESOURCES">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Blackboard Nepal</title>
+	<title>Blackboard Nepal | Supporting the community| A platform for all Nepalese communities </title>
 </head>
 
 <body>
-    <div class="tagline">SUPPORTING THE COMMUNITY</div>
-	<div class="header">
+   <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJB6J4S"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+    <p class="tagline">SUPPORTING THE COMMUNITY</p>
+	<header class="header">
+	    <a href="index.php"> <div class="logo_container">
+				</div> </a>
 		<div class ="navigation">
 			<a href="contact_form.html">CONTACT</a>
 			<a href="about.html">ABOUT</a>
 			<a href="naaticclnepali.html">NAATI</a>
 			<a href="index.php">HOME</a>
 		</div>	
-		<a href="index.php"> <div class="logo">
-				Blackboard Nepal</div> </a>
-	</div>
+	</header>
 	
-    <div class="sublink_container">
-    	<div id="naaticcl"class="sublink_odd">
-    		<button class="button_sublinks" onclick="location.href='naaticclnepali.html'">NAATI CCL</button>
-    		<p class ="text_sublinks"> Naati CCL test is a test of your interpretation skills in a LOTE language. This test is one of the important aspect of Australian immigration as passing this test adds extra 5 points to PR points. This test can be daunting especially if you are thinking of preparing for this test at home. To help you with your preparation, we have compiled some resources for you. We have some tips for preparation, collection of vocabulary lists and information on test formats. If you have any suggestions or feedback, please write back to us. </p>
-    	</div>
-    	    <div class="adboard_odd">
-    		</div>
-    </div>
     
-    <div class="sublink_container">
+    	<div id="naaticcl"class="sublink_odd">
+    		<button class="button_sublinks" onclick="location.href='naaticclnepali.html'">Naati CCL</button>
+    		<p class ="text_sublinks">Naati CCL test is a test of your interpretation skills in a language other than English. This test is one of the important aspect of Australian immigration as passing this test adds extra 5 points to Permanent Residency points. This test can be daunting especially if you are thinking of preparing for this test at home. To support you with your preparation, we have compiled some resources for you. We have some tips for preparation, collection of vocabulary lists and information on test formats. If you have any suggestions or feedback, please write to us. </p>
+    	</div>
+
     	<div id ="about"class="sublink_odd">
     		<h1>ABOUT BLACKBOARD NEPAL</h1><br>
-    		<p class ="text_sublinks"> The concept of BlackBoard Nepal emerged from the sense of need of a community platform for all of us Nepalese people living outside our country. It is an idea that binds us Nepalese for who we are, how we are and wherever we are. Currently, this platform is for all Nepalese people living in Australia. With time, we will be there for all Nepalese throughout the world. </p>
-    	</div>	
-    	<div class="adboard_odd">
-    		
+    		<p class ="text_sublinks"> The concept of BlackBoard Nepal emerged from the sense of need of a community platform for all of us Nepalese people living outside our country. It is an idea that binds us Nepalese for who we are, how we are and wherever we are. Currently, this platform is for all Nepalese people living in Australia. With time, we will expand our platform for all Nepalese throughout the world. We have Naati Support materials at the moment. We will be adding other helpful supporting materials to help Nepalese people living throughout the world. </p>
     	</div>
-    </div>
     
     
-    <div id="footer" class="footer">
+    <footer id="footer" class="footer">
 		<div class="column">
-				<p>&copy2020, Blackboard Nepal</p>
+				<p>&copy;2020|Blackboard Nepal|</p>
 			</div>
 			<div class="column">
 				<a href="privacy_policy.html"><p>Privacy Policy</p></a>
 				<a href="contact_form.html"><p>Contact Us</p></a>
 				<a href="about.html"><p>About Us</p></a>
 			</div>
-	</div>
+	</footer>
 </body>
 
 </html>
